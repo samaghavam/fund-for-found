f12cb63e566fd68c08e92e622fc744be777b35fe
sama <sghavamzadeh46@gmail.com>
2024-10-14 10:54:05 +0330

    create input component

--- 3f-brands/app/page.tsx
+++ 3f-brands/app/page.tsx
@@ -4,6 +4,9 @@ import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
+import Inputs from "@/components/shared/Input";
+import { LuSearch } from "react-icons/lu";
+
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -31,6 +34,7 @@ export default async function Index() {
       <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
+          <Inputs placeholder="Search brand, category, tag or..." icon={LuSearch} />
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
           {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
         </main>
